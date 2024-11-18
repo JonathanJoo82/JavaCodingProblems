6f899dc2923f121c5f53cc57b13f8e71ab607529
JonathanJoo82 <jonathanjoo@gmail.com>
2024-11-18 13:05:11 -0600

    Question 1 Chapter 1

--- JavaCodingProblems/src/Chapter1/Chapter1Solutions.java
+++ JavaCodingProblems/src/Chapter1/Chapter1Solutions.java
@@ -1,4 +1,22 @@
 package Chapter1;
 
+import java.io.CharConversionException;
+import java.util.HashMap;
+
 public class Chapter1Solutions {
+
+    //count the given duplicates of characters in a given string
+    public static void countDuplicateCharacters(String str){
+        HashMap<Character, Integer> duplicatedCharactersMap = new HashMap<>();
+        str.chars().forEach(ch -> {
+            System.out.println((char) ch);
+            if(duplicatedCharactersMap.containsKey((char)ch)){
+                int count = duplicatedCharactersMap.get((char)ch);
+                count++;
+                duplicatedCharactersMap.replace((char)ch, count);
+            } else {
+                duplicatedCharactersMap.put((char) ch, 1);
+            }
+        });
+    }
 }
