9bceeaec9860e48f5867ea406aaff1856b1be8e2
JonathanJoo82 <jonathanjoo@gmail.com>
2024-11-19 13:37:17 -0600

    Problem 2

--- JavaCodingProblems/src/Chapter1/Chapter1Solutions.java
+++ JavaCodingProblems/src/Chapter1/Chapter1Solutions.java
@@ -1,7 +1,11 @@
 package Chapter1;
 
 import java.io.CharConversionException;
+import java.security.Key;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Chapter1Solutions {
 
@@ -9,7 +13,6 @@ public class Chapter1Solutions {
     public static void countDuplicateCharacters(String str){
         HashMap<Character, Integer> duplicatedCharactersMap = new HashMap<>();
         str.chars().forEach(ch -> {
-            System.out.println((char) ch);
             if(duplicatedCharactersMap.containsKey((char)ch)){
                 int count = duplicatedCharactersMap.get((char)ch);
                 count++;
@@ -19,4 +22,22 @@ public class Chapter1Solutions {
             }
         });
     }
+
+    //uses the merge function to check if the previous value of the key is present if not present it will replace it with updated value count.
+    public static void coPilotCountDuplicateCharacters(String str){
+        HashMap<Character, Integer> duplicatedCharactersMap = new HashMap<>();
+        str.chars().forEach(ch -> duplicatedCharactersMap.merge((char) ch, 1, Integer::sum));
+    }
+
+    //program that returns the first non-repeated character from a given string.
+    public static void returnFirstNonRepeatedCharacter(String str){
+        HashMap<Character, Integer> duplicatedCharactersMap = new HashMap<>();
+        str.chars().forEach(ch -> duplicatedCharactersMap.merge((char) ch, 1, Integer::sum));
+
+        List<Character> keysToRemoveList = duplicatedCharactersMap.entrySet().stream().filter(entry -> entry.getValue() > 1).map(Map.Entry :: getKey).collect(Collectors.toList());
+        keysToRemoveList.forEach(duplicatedCharactersMap::remove);
+        System.out.println(duplicatedCharactersMap);
+    }
+    //reverses the letter of each word and the word themselves.
+
 }
--- JavaCodingProblems/src/Main.java
+++ JavaCodingProblems/src/Main.java
@@ -3,5 +3,6 @@ import Chapter1.Chapter1Solutions;
 public class Main {
     public static void main(String[] args) {
         Chapter1Solutions.countDuplicateCharacters("testing");
+        Chapter1Solutions.returnFirstNonRepeatedCharacter("testing");
     }
 }
